01b9971ba97666d260930db3290a9b9ce3704782
slaughterb <slaughtb@uci.edu>
2024-03-03 22:19:57 -0800

    Updates

---
+++ precache-manifest.b4eb74b8a6b8c754b07cb603de61bf26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc4a6edcdb98ed5d04658c96537bacd",
+    "url": "/react-cards/index.html"
+  },
+  {
+    "revision": "fb07235238c2ec3adf31",
+    "url": "/react-cards/static/css/main.34b00e1e.chunk.css"
+  },
+  {
+    "revision": "3982cf3831b813abf23f",
+    "url": "/react-cards/static/js/2.47f01391.chunk.js"
+  },
+  {
+    "revision": "2133a33f4e9282dbf343",
+    "url": "/react-cards/static/js/3.c60c86f6.chunk.js"
+  },
+  {
+    "revision": "fb07235238c2ec3adf31",
+    "url": "/react-cards/static/js/main.c7a2e31c.chunk.js"
+  },
+  {
+    "revision": "d18cde9206204b68f940",
+    "url": "/react-cards/static/js/runtime~main.ed848db0.js"
+  },
+  {
+    "revision": "60b1f37aa15cc63041adc78da03c0cad",
+    "url": "/react-cards/static/media/pexels-notebg.60b1f37a.jpg"
+  }
+]);
